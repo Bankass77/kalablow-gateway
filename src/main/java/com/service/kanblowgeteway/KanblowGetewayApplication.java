67c822482d5cc684c8f7039e664c91592219c5cb
Bankass77 <amguindo77@gmail.com>
2026-09-02 18:20:34 +0200

    first commit

---
+++ src/main/java/com/service/kanblowgeteway/KanblowGetewayApplication.java
@@ -0,0 +1,13 @@
+package com.service.kanblowgeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanblowGetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanblowGetewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/service/kanblowgeteway/config/CorsConfig.java
@@ -0,0 +1,4 @@
+package com.service.kanblowgeteway.config;
+
+public class CorsConfig {
+}
